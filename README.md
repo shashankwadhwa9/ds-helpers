55a746cdac09dc7ece53560ad198e20202c9aed2
Shashank Wadhwa <shashankwadhwa9@gmail.com>
2018-12-09 13:22:11 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# What is it?
+
+ds_helpers is a Python package providing common helper functions for quick data analysis and modeling. Now you can simply call the helper functions for repetitive work and focus on other important things in your Data Science project.
+
+# Where to get it
+
+The source code is currently hosted on GitHub at: <https://github.com/shashankwadhwa9/ds_helpers>
+
+Binary installers for the latest released version are available at the Python package index.
+
+    pip install ds_helpers
+
+# License
+[MIT](https://github.com/shashankwadhwa9/ds_helpers/blob/master/LICENSE)
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='ds_helpers',
+    version='0.0.1',
+    author='Shashank Wadhwa',
+    author_email='shashankwadhwa9@gmail.com',
+    description='Helper functions for Data analysis and modeling',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/shashankwadhwa9/ds_helpers',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
