dd57c8116b84989aeead10f5c7dc6f09660700a5
Shashank Wadhwa <shashankwadhwa9@gmail.com>
2018-12-11 19:04:00 +0530

    v0.0.4: fix import path

--- ds_helpers/__init__.py
+++ ds_helpers/__init__.py
@@ -1,4 +1,4 @@
-from helpers import (
+from ds_helpers.helpers import (
     get_null_value_counts, plot_categorical_column, plot_continuous_column, evaluate_models, encode_categorical,
     get_scaled_df, get_feature_importances
 )
