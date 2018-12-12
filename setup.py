72d765960bc0faeec058cae21ef93789c6cbd30d
Shashank Wadhwa <shashankwadhwa9@gmail.com>
2018-12-12 18:43:27 +0530

    Change version to 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='ds_helpers',
-    version='0.0.6',
+    version='0.1.0',
     author='Shashank Wadhwa',
     author_email='shashankwadhwa9@gmail.com',
     description='Helper functions for Data analysis and modeling',
